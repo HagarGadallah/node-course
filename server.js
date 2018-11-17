de0df7715fcd1706a10610bfa65f0d5abd7a3539
HagarGadallah <hagargadallah@gmail.com>
2018-11-17 23:16:43 +0200

    err fixed + projects page

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use((req, res, next) => {
   var log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log', log + '\n');
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log("Unable to append to log file");
+    }
+  });
   next();
 });
 
@@ -43,6 +47,12 @@ app.get('/about', (req, res) => {
     pageTitle: 'About Page'
   });
 });
+
+app.get('/projects', (req,res) => {
+	res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+})
 
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
